71ae9f1a182c7be355ca1a8adfb28be9a7f11796
inhooo00 <chldlsgh0987@naver.com>
2025-03-21 01:44:58 +0900

    first commit

---
+++ src/main/java/shop/itcontest17/itcontest17/ItContest17Application.java
@@ -0,0 +1,13 @@
+package shop.itcontest17.itcontest17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItContest17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItContest17Application.class, args);
+    }
+
+}
---
+++ src/test/java/shop/itcontest17/itcontest17/ItContest17ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.itcontest17.itcontest17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItContest17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
