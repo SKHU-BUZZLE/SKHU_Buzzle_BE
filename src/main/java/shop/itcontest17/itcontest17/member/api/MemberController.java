f6d9c8ba6240a8c380102deddb4cf837a2f9d904
inhooo00 <chldlsgh0987@naver.com>
2025-03-21 12:59:55 +0900

    랭킹 조회 swagger 설명 추가

--- src/main/java/shop/itcontest17/itcontest17/member/api/MemberController.java
+++ src/main/java/shop/itcontest17/itcontest17/member/api/MemberController.java
@@ -1,5 +1,10 @@
 package shop.itcontest17.itcontest17.member.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +22,13 @@ public class MemberController {
 
     final private MemberService memberService;
 
+    @Operation(summary = "상위 10등 조회", description = "상위 10등 유저를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "응답 생성을 성공했습니다"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청입니다"),
+            @ApiResponse(responseCode = "401", description = "헤더 없음 or 토큰 불일치",
+                    content = @Content(schema = @Schema(example = "INVALID_HEADER or INVALID_TOKEN")))
+    })
     @GetMapping("/ranking")
     public RspTemplate<List<Member>> getTop10MembersByStreak() {
         return new RspTemplate<>(HttpStatus.OK, "상위 10등 조회 완료", memberService.getTop10MembersByStreak());
