4be86bdbd405724ba1e7615a41d729f33b506b99
inhooo00 <chldlsgh0987@naver.com>
2025-03-24 13:28:07 +0900

    내정보 조회 dosc 추가

--- src/main/java/shop/itcontest17/itcontest17/member/api/MemberDocs.java
+++ src/main/java/shop/itcontest17/itcontest17/member/api/MemberDocs.java
@@ -7,8 +7,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import shop.itcontest17.itcontest17.global.annotation.CurrentUserEmail;
 import shop.itcontest17.itcontest17.global.template.RspTemplate;
 import shop.itcontest17.itcontest17.member.api.dto.response.MemberInfoListDto;
+import shop.itcontest17.itcontest17.member.api.dto.response.MemberInfoResDto;
 import shop.itcontest17.itcontest17.member.api.dto.response.MemberLifeResDto;
 import shop.itcontest17.itcontest17.member.domain.Member;
 
@@ -34,5 +36,16 @@ public interface MemberDocs {
                     @ApiResponse(responseCode = "500", description = "서버 오류")
             })
     RspTemplate<MemberLifeResDto> getMemberLife(
+            @Parameter(description = "로그인한 유저의 이메일(토큰에서 자동 추출)", hidden = true) String email);
+
+    @Operation(summary = "내 정보 조회", description = "내 정보를 조회합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "유저 조회 성공",
+                            content = @Content(schema = @Schema(implementation = MemberInfoResDto.class))),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "401", description = "인증 실패"),
+                    @ApiResponse(responseCode = "500", description = "서버 오류")
+            })
+    RspTemplate<MemberInfoResDto> getMyPage(
             @Parameter(description = "로그인한 유저의 이메일(토큰에서 자동 추출)", hidden = true) String email);
 }
